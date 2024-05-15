def8de9a3cb2d571bdf2a2f865e535819422f161
detodavide <davide.marchetti931@gmail.com>
2024-05-15 15:17:05 +0200

    fix

--- llm/langgraph/csv_point_selection/nodes.py
+++ llm/langgraph/csv_point_selection/nodes.py
@@ -9,6 +9,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_groq import ChatGroq
 from utils.logger import logger
+from llm.templates.llama_templates import TEMPLATE_1
 
 def generate_questions(state, llm):   
     """Take the initial df and input_data to generate the questions based on the data"""
@@ -18,21 +19,21 @@ def generate_questions(state, llm):
     num_steps = int(state["num_steps"])
     num_steps += 1
 
+    system_template = """You are a Data Analyst Agent that is an expert on making insightful questions."""
+    user_template = """Given the following dataframe: {df}\n
+        Write 20 questions that focus on the INPUT DATA in relation to the whole dataframe ( correlations, meaningful insights ...), no preamble or explanation.
+
+        INPUT DATA:{input_data}\n\n"""
+    
     prompt = PromptTemplate(
-    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
-    You are a Data Analyst Agent that is an expert on making insightful questions.
-
-    <|eot_id|><|start_header_id|>user<|end_header_id|>
-    Given the following dataframe: {df}\n
-    Write 20 questions that focus on the INPUT DATA in relation to the whole dataframe ( correlations, meaningful insights ...), no preamble or explanation.
-
-    INPUT DATA:{input_data}\n\n
-    <|eot_id|>
-    <|start_header_id|>assistant<|end_header_id|>
-    """,
-    input_variables=["df", "input_data"],
+        input_variables=["df", "input_data"],
+        template=TEMPLATE_1.format(
+            system_prompt=system_template,
+            user_prompt=user_template
+        )
     )
 
+    print(prompt)
     questions_generator = prompt | llm | StrOutputParser()
     questions = questions_generator.invoke({"df": df, "input_data": input_data})
 
@@ -50,22 +51,22 @@ def questions_answering(state, llm):
     questions = state["questions"]
     num_steps += 1
 
-    prompt = PromptTemplate(
-    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
-        You are a Data Analyst expert that is able to find meaningful insights answering the user questions about data.
 
-        <|eot_id|><|start_header_id|>user<|end_header_id|>
-        DATAFRAME: {df}\n
+    system_template = """You are a Data Analyst expert that is able to find meaningful insights answering the user questions about data."""
+    user_template = """DATAFRAME: {df}\n
         INPUT DATA: {input_data}\n
         QUESTIONS: {questions}\n
 
-        Answer each questions giving a proper analysis and explanation on how the INPUT DATA compares to the whole DATAFRAME, giving a strong focues on the INPUT DATA.
-        <|eot_id|>
-        <|start_header_id|>assistant<|end_header_id|>
-        """,
+        Answer each questions giving a proper analysis and explanation on how the INPUT DATA compares to the whole DATAFRAME, giving a strong focues on the INPUT DATA."""
+    
+    prompt = PromptTemplate(
+        template=TEMPLATE_1.format(
+            system_prompt=system_template,
+            user_prompt=user_template
+        ),
         input_variables=["df", "input_data", "questions"],
     )
-
+    print(prompt)
     answers_generator = prompt | llm | StrOutputParser()
     answers = answers_generator.invoke({"df": df, "input_data": input_data, "questions": questions})
     logger.info(f"Generated answers: {answers}")
@@ -81,21 +82,20 @@ def summarize_answers(state,llm):
     num_steps = int(state["num_steps"])
     num_steps += 1
     
-    prompt = PromptTemplate(
-    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
-        You are a Data Analyst expert that is a master on summarize informations and return it in italian only.
-
-        <|eot_id|><|start_header_id|>user<|end_header_id|>
-        ANSWERS: {answers}\n
+    system_template = """You are a Data Analyst expert that is a master on summarize informations and return it in italian only."""
+    user_template ="""ANSWERS: {answers}\n
 
         Summarize the whole answers in a very discorsive single paragraph without adding any preamble or introduction.
         Just answer with the summary in a non-technical way like you are talking to the common user.
 
         You can use this critics if present, to correct your output:
-        CRITICS: {summary_critics}\n
-        <|eot_id|>
-        <|start_header_id|>assistant<|end_header_id|>
-        """,
+        CRITICS: {summary_critics}\n"""
+
+    prompt = PromptTemplate(
+        template=TEMPLATE_1.format(
+            system_prompt=system_template,
+            user_prompt=user_template
+        ),
         input_variables=["answers", "summary_critics"],
     )
 
@@ -117,19 +117,18 @@ def summarize_critics(state, llm):
     num_steps += 1
     critics_steps += 1
 
-    prompt = PromptTemplate(
-    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
-        You are an expert on critic a text and giving useful critical insights.
-
-        <|eot_id|><|start_header_id|>user<|end_header_id|>
-        Given a SUMMARY on some ANSWERS, return a insightful critics to better the SUMMARY, remind that the summary must be short and aknowledge the focus on the INPUT DATA.\n
+    system_template = """You are an expert on critic a text and giving useful critical insights."""
+    user_template = """Given a SUMMARY on some ANSWERS, return a insightful critics to better the SUMMARY, remind that the summary must be short and aknowledge the focus on the INPUT DATA.\n
 
         SUMMARY: {summary}\n
         ANSWERS: {answers}\n
-        INPUT DATA: {input_data}
-        <|eot_id|>
-        <|start_header_id|>assistant<|end_header_id|>
-        """,
+        INPUT DATA: {input_data}"""
+
+    prompt = PromptTemplate(
+        template=TEMPLATE_1.format(
+            system_prompt=system_template,
+            user_prompt=user_template
+        ),
         input_variables=["answers", "summary", "input_data"],
     )
 
---
+++ llm/templates/llama_templates.py
@@ -0,0 +1,6 @@
+TEMPLATE_1 = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+        {system_prompt}
+        <|eot_id|><|start_header_id|>user<|end_header_id|>
+        {user_prompt}
+        <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+        <|start_header_id|>assistant<|end_header_id|>"""
